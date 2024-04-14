9be11e0ca686b118c2a0c0cba8bb06059dda114a
Aleksei Naumov <mail@anaumov.com>
2024-04-14 14:04:40 +0300

    Add an empty project  and configuration files

---
+++ jest.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+  roots: ['<rootDir>/src'],
+  testMatch: [
+    '**/__tests__/**/*.+(ts|tsx|js)',
+    '**/?(*.)+(spec|test).+(ts|tsx|js)',
+  ],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+};
